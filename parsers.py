4e5b3ad21df65791276cc31847aa3b493f68c250
saddexed <seta5869@gmail.com>
2022-10-04 16:37:22 +0530

    Add Messenger and slight code change

--- parsers.py
+++ parsers.py
@@ -2,21 +2,21 @@ import re
 
 def twitter(link):
     if f"/status/" in link:
-        m = (re.search(r"twitter.com\/[\w\-.]{3,}\/status\/[\d]{3,}", link)).group(0)
-        stat = (re.search(r"[\d]{3,}", link)).group(0)
+        m = (re.search(r"twitter.com\/[\w\-.]+\/status\/[\d]+", link)).group(0)
+        stat = (re.search(r"[\d]+", link)).group(0)
         return {"web": m, "android": f"intent://{m}#Intent;package=com.twitter.android;scheme=https;end", "ios": f"twitter://status?id={stat}"}
     else:
-        m = (re.search(r"twitter.com\/[\w\-.]{3,}", link)).group(0)
+        m = (re.search(r"twitter.com\/[\w\-.]+", link)).group(0)
         return {"web": m, "android": f"intent://{m}#Intent;package=com.twitter.android;scheme=https;end", "ios": f"twitter://user?screen_name={m[12:]}"}
 def youtube(link):
-    m = (re.search(r"youtube.com\/watch\?v\=[\w\-]{3,}", link)).group(0)
+    m = (re.search(r"youtube.com\/watch\?v\=[\w\-]+", link)).group(0)
     return {"web": m, "android": f"intent://{m}#Intent;package=com.google.android.youtube;scheme=https;end", "ios": f"vnd.youtube://{m[20:]}"}   
 def instagram(link):
     if f"/p/" in link:
-        m = (re.search(r"instagram.com\/p\/[\w\-\.]{3,}", link)).group(0)
+        m = (re.search(r"instagram.com\/p\/[\w\-\.]+", link)).group(0)
         return {"web": m, "android": f"intent://{m}#Intent;package=com.instagram.android;scheme=https;end", "ios": f"instagram://media?id={m[16:]}"}
     else:
-        m = (re.search(r"instagram.com\/[\w\-\.]{3,}", link)).group(0)
+        m = (re.search(r"instagram.com\/[\w\-\.]+", link)).group(0)
         return {"web": m, "android": f"intent://{m}#Intent;package=com.instagram.android;scheme=https;end", "ios": f"instagram://user?username={m[14:]}"}
 def snapchat(link):
     if f"/add/" in link:
@@ -25,7 +25,9 @@ def snapchat(link):
     else:
         m = (re.search(r"snapchat.com\/discover\/[\w\.]+", link)).group(0)
         return {"web": m, "android": f"intent://discover/{m[22:]}#Intent;package=com.snapchat.android;scheme=snapchat;end;", "ios": f"snapchat://discover/{m[22:]}"}
-
+def messenger(link):
+    m = (re.search(r"messenger.com\/t\/[\d\.]+", link)).group(0)
+    return {"web": m, "android": f"intent://user/{m[16:]}/#Intent;package=com.facebook.orca;scheme=fb-messenger;end", "ios": f"fb-messenger-public://user-thread/{m[16:]}"}
 
 
 
