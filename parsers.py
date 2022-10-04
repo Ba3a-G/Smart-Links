c40d54ddd3026b5e7bc406ea7d34d9fb9e66aea9
saddexed <seta5869@gmail.com>
2022-10-04 12:01:00 +0530

    Add Snapchat and slight code cleanup

--- parsers.py
+++ parsers.py
@@ -8,11 +8,9 @@ def twitter(link):
     else:
         m = (re.search(r"twitter.com\/[\w\-.]{3,}", link)).group(0)
         return {"web": m, "android": f"intent://{m}#Intent;package=com.twitter.android;scheme=https;end", "ios": f"twitter://user?screen_name={m[12:]}"}
-
 def youtube(link):
     m = (re.search(r"youtube.com\/watch\?v\=[\w\-]{3,}", link)).group(0)
-    return {"web": m, "android": f"intent://{m}#Intent;package=com.google.android.youtube;scheme=https;end", "ios": f"vnd.youtube://{m[20:]}"}
-        
+    return {"web": m, "android": f"intent://{m}#Intent;package=com.google.android.youtube;scheme=https;end", "ios": f"vnd.youtube://{m[20:]}"}   
 def instagram(link):
     if f"/p/" in link:
         m = (re.search(r"instagram.com\/p\/[\w\-\.]{3,}", link)).group(0)
@@ -20,5 +18,15 @@ def instagram(link):
     else:
         m = (re.search(r"instagram.com\/[\w\-\.]{3,}", link)).group(0)
         return {"web": m, "android": f"intent://{m}#Intent;package=com.instagram.android;scheme=https;end", "ios": f"instagram://user?username={m[14:]}"}
+def snapchat(link):
+    if f"/add/" in link:
+        m = (re.search(r"snapchat.com\/add\/[\w\.]+", link)).group(0)
+        return {"web": m, "android": f"intent://add/{m[16:]}#Intent;package=com.snapchat.android;scheme=snapchat;end;", "ios": f"snapchat://add/{m[16:]}"}
+    else:
+        m = (re.search(r"snapchat.com\/discover\/[\w\.]+", link)).group(0)
+        return {"web": m, "android": f"intent://discover/{m[22:]}#Intent;package=com.snapchat.android;scheme=snapchat;end;", "ios": f"snapchat://discover/{m[22:]}"}
+
+
+
 
 #return {"web": m, "android": f"", "ios": f""}
